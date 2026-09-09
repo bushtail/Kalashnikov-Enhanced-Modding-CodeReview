28926e527b48bd2f09f815e520af39bed51e7d6b
danauraborealis <smittywerbenjagermanjensen40@gmail.com>
2026-09-09 17:49:53 -0500

    updates for forge compliance

--- KalashnikovEnhancedModdingServer/Mod.cs
+++ KalashnikovEnhancedModdingServer/Mod.cs
@@ -19,16 +19,18 @@ namespace KalashnikovEnhancedModding;
 
 public record ModMetadata : IModMetadata
 {
-    public string ModGuid { get; init; } = "com.baliston.kalashnikov-enhanced-modding";
-    public string Name { get; init; } = "Kalashnikov Enhanced Modding";
-    public string Author { get; init; } = "BALIST0N, ChoccyMilk & Gatsu667";
-    public List<string>? Contributors { get; init; }
-    public Version Version { get; init; } = new("4.1.1");
+    private static string BuildValue(string key) => typeof(ModMetadata).Assembly
+        .GetCustomAttributes<AssemblyMetadataAttribute>().Single(attribute=>attribute.Key==key).Value!;
+    public string ModGuid { get; init; } = BuildValue("ModGuid");
+    public string Name { get; init; } = BuildValue("ModName");
+    public string Author { get; init; } = BuildValue("ModAuthor");
+    public List<string>? Contributors { get; init; } = ["ChoccyMilk", "Gatsu667"];
+    public Version Version { get; init; } = new(BuildValue("ModVersion"));
     public Range SptVersion { get; init; } = new("~4.1.0");
     public bool HasPrepatcher { get; init; }
     public List<string>? Incompatibilities { get; init; }
     public Dictionary<string, Range>? ModDependencies { get; init; } = new() { ["com.wtt.commonlib"] = new(">=3.0.6 <4.0.0") };
-    public string? Url { get; init; } = "https://github.com/BALIST0N/AkEnhancedModding";
+    public string? Url { get; init; } = BuildValue("ModSourceUrl");
     public string License { get; init; } = "NCSA";
 }
 
